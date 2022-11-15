e11670640d9db3781c78e32e11cc1c71d0d5e61d
chungchung <bekurin17@gmail.com>
2022-11-15 09:47:16 +0900

    Feat: "추가 도커 이미지 빌드 후 도커 허브 업로드 테스트를 위한 프로젝트"

---
+++ ci-with-github-actions/src/test/kotlin/com/core/ciwithgithubactions/CiWithGithubActionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.core.ciwithgithubactions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CiWithGithubActionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
